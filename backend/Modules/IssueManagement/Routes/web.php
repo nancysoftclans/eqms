4fb263b3028fc9671e961cf43e31219982be6153
Chris Watia <chriswatia@gmail.com>
2024-07-26 10:18:55 +0300

    Quality Review Updates

--- backend/Modules/IssueManagement/Routes/web.php
+++ backend/Modules/IssueManagement/Routes/web.php
@@ -19,7 +19,8 @@ Route::group(['prefix' => 'issuemanagement', 'middleware' => ['web']], function 
     Route::get('getIssueManagementDetails', [IssueManagementController::class, 'getIssueManagementDetails']);
     Route::get('getIssueManagementDetailsById/{active_application_id}', [IssueManagementController::class, 'getIssueManagementDetailsById']);
     Route::get('getIssueProcessDetails', [IssueManagementController::class, 'getIssueProcessDetails']);
-    Route::post('saveIssueQualityReviewDetails', [IssueManagementController::class, 'saveIssueQualityReviewDetails']);
+    Route::post('saveIssueInitialQualityReviewDetails', [IssueManagementController::class, 'saveIssueInitialQualityReviewDetails']);
     Route::post('saveIssueRCADetails', [IssueManagementController::class, 'saveIssueRCADetails']);
     Route::post('saveIssueResolutionwDetails', [IssueManagementController::class, 'saveIssueResolutionwDetails']);
+    Route::post('saveIssueQualityReviewDetails', [IssueManagementController::class, 'saveIssueQualityReviewDetails']);
 });
--- classic/src/view/issuemanagement/views/panels/IssueQualityReviewPnl.js
+++ classic/src/view/issuemanagement/views/panels/IssueQualityReviewPnl.js
@@ -1,12 +1,72 @@
-Ext.define("Admin.view.issuemanagement.views.panels.IssueQualityReviewPnl", {
-  extend: "Ext.panel.Panel",
-  xtype: "issuequalityofficereview",
-  controller: "issuemanagementvctr",
-  viewModel: "issuemanagementvm",
-  layout: "fit",
-  items: [
-    {
-      xtype: "issuereceivingwizard",
+Ext.define(
+  "Admin.view.issuemanagement.views.panels.IssueQualityReviewPnl",
+  {
+    extend: "Ext.tab.Panel",
+    xtype: "issuequalityreviewpnl",
+    layout: {
+      type: "fit",
     },
-  ],
-});
+    defaults: {
+      margin: 3,
+    },
+
+    viewModel: {
+      type: "issuemanagementvm",
+    },
+    items: [
+      {
+        xtype: "issueresolutionfrm",
+        autoScroll: true,
+        title: "Resolution",
+        fieldDefaults: {
+          fieldStyle: {
+            color: "green",
+            "font-weight": "bold",
+          },
+        },
+      },
+      {
+        xtype: "issueinitialqualityreviewfrm",
+        autoScroll: true,
+        title: "Initial Review by Quality Office",
+        fieldDefaults: {
+          fieldStyle: {
+            color: "green",
+            "font-weight": "bold",
+          },
+        },
+      },
+      {
+        xtype: "issuemanagementfrm",
+        autoScroll: true,
+        title: "Issue Details",
+        fieldDefaults: {
+          fieldStyle: {
+            color: "green",
+            "font-weight": "bold",
+          },
+        },
+      },
+      {
+        xtype: "complainantdetailsfrm",
+        autoScroll: true,
+        title: "Complainant Details",
+        fieldDefaults: {
+          fieldStyle: {
+            color: "green",
+            "font-weight": "bold",
+          },
+        },
+      },
+      {
+        xtype: "issuemanagementdocuploadsgrid",
+        title: "Documents",
+      },
+      {
+        xtype: "hiddenfield",
+        name: "_token",
+        value: token,
+      },
+    ],
+  }
+);
